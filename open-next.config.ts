d67d40440b55d2e377c65f81dcfcbac8a71654af
gnarantsend-oss <gnarantsend@gmail.com>
2026-04-11 16:41:27 +0800

    delete

--- open-next.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-
-export default defineCloudflareConfig({
-  // R2 cache-г ашиглах бол доорх мөрийг тайлбарыг арилгаарай:
-  // incrementalCache: r2IncrementalCache,
-  // Үүний тулд: import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
-  // мөн wrangler.jsonc-д r2_buckets тохиргоо нэмэх шаардлагатай
-});
